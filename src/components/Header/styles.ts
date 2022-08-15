fdd70baef9ee43055277d43c289128d19ffcc3bd
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-08-14 23:11:47 -0300

    chore: add screens size to themes

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
-  background: ${(props) => props.theme["gray-900"]};
+  background: ${(props) => props.theme.colors["gray-900"]};
   padding: 2.5rem 0 7.5rem;
 `;
 
 export const HeaderContent = styled.div`
   width: 100%;
-  max-width: 1120px;
+  max-width: ${(props) => props.theme.screens.desktop};
   margin: 0 auto;
   padding: 0 1.5rem;
   display: flex;
@@ -18,14 +18,14 @@ export const HeaderContent = styled.div`
 export const NewTransactionButton = styled.button`
   height: 50px;
   border: 0;
-  background: ${(props) => props.theme["green-500"]};
-  color: ${(props) => props.theme["white"]};
+  background: ${(props) => props.theme.colors["green-500"]};
+  color: ${(props) => props.theme.colors["white"]};
   font-weight: bold;
   padding: 0 1.25rem;
   border-radius: 6px;
   cursor: pointer;
   &:hover {
-    background: ${(props) => props.theme["green-700"]};
+    background: ${(props) => props.theme.colors["green-700"]};
     transition: background-color 0.2s;
   }
 `;
