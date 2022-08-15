487dcbd6a77bee8f42a7eb0f2ce5a5421f146c30
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-08-14 23:36:36 -0300

    feat: create table component layout

---
+++ src/components/Table/index.tsx
@@ -0,0 +1,32 @@
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "../../components/Table/styles";
+
+export function Table() {
+  return (
+    <TransactionsContainer>
+      <TransactionsTable>
+        <tbody>
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+            <td>
+              <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>10/05/2022</td>
+          </tr>
+          <tr>
+            <td width="50%">Hambúrguer</td>
+            <td>
+              <PriceHighlight variant="outcome">-R$ 59,00</PriceHighlight>
+            </td>
+            <td>Alimentação</td>
+            <td>12/05/2022</td>
+          </tr>
+        </tbody>
+      </TransactionsTable>
+    </TransactionsContainer>
+  );
+}
