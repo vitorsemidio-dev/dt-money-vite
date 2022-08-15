f534f3bb8c24b536e02a62e14ba136de6f07b888
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-08-14 23:27:44 -0300

    feat: create summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+
 import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
